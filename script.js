d4e1d1cdbe1504973be4cfb2d884e575fc4668a3
Likhaya Gashe <likhayagashe@gmail.com>
2025-07-10 17:38:50 +0200

    feat: Remove unused Firebase scripts and related functionality

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleRules() {
-  const modal = document.getElementById("rulesModal");
-  if (modal.style.display === "block") {
-    modal.style.display = "none";
-  } else {
-    modal.style.display = "block";
-  }
-}
